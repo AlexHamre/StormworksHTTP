9f138c1f793338953ae09572aec5fbf0e07a228d
Alexander Hamre <91455968+AlexHamre@users.noreply.github.com>
2022-10-20 22:44:59 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,32 +1,33 @@
 var http = require('http');
 var fs = require('fs');
-var values = []
+var numbers = ["69","0"];
+var bools = ["1","0"];
 
 http.createServer(function (req, res) {
     console.log(req.url)
 
-    if (req.url.indexOf("&timeout") > -1){
-      s = req.url.indexOf("&")
-      x = req.url.slice(7,s)
-        console.log(values[x])
-      res.write(values[x])
-      return res.end()
+    if (req.url.indexOf("/?stormnum") > -1){
+        res.write(numbers.toString())
+        return res.end()
     }
 
-    if (req.url.indexOf("/?storm") > -1){
-      x = req.url.slice(7)
-      console.log(values[x])
-      if (values[x] == undefined){
-        values[x] = "69"
-      }
-      res.write(values[x])
+    else if (req.url.indexOf("/?stormbool") > -1){
+      res.write(bools.toString())
       return res.end()
-    }
+  }
+
     else if (req.url.indexOf("/?val") > -1){
       s = req.url.indexOf("=")
       x = req.url.slice(5, s)
-      values[x] = req.url.slice(s+1)
-      res.write(values[x])
+      numbers[x] = req.url.slice(s+1)
+      res.write(numbers[x])
+      return res.end()
+    }
+    else if (req.url.indexOf("/?bool") > -1){
+      s = req.url.indexOf("=")
+      x = req.url.slice(6, s)
+      bools[x] = req.url.slice(s+1)
+      res.write(bools[x])
       return res.end()
     }
     else if (req.url == "/style.css") {
@@ -41,10 +42,10 @@ http.createServer(function (req, res) {
       res.write(data); return res.end(); }); 
     }
     else{
-    fs.readFile('newer_controls.html', function(err, data) {
+    fs.readFile('controls.html', function(err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         return res.end();
       });
     }
-    }).listen(3000);
+    }).listen(80);
--- sliders.js
+++ sliders.js
@@ -10,3 +10,26 @@ sliders.forEach(slider => {
         xhttp.send();
     })
 });
+
+const buttons = document.getElementsByClassName("button")
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", buttonClickFunction)
+    console.log(buttons[i]);
+}
+clicked = []
+function buttonClickFunction(){
+    if (clicked[this.id] != true){
+        clicked[this.id] = true
+        this.style="background-color: white;"
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "/?bool"+ this.id +"="+ "1", true);
+        xhttp.send();
+    }
+    else{
+        clicked[this.id] = false
+        this.style="background-color: red;"
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "/?bool"+ this.id +"="+ "0", true);
+        xhttp.send();
+    }
+}
